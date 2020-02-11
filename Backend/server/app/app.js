f21cad860c8e9d17a90f90c7e436b34542edfab5
Kalen <ktwmgoo@gmail.com>
2020-02-10 21:40:36 -0800

    Created connection from firebase to the node server, ran a simple query to print all users

--- Backend/server/app/app.js
+++ Backend/server/app/app.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = 3000;
+const firebase = require("../../firebase");
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));
+app.get("/", (req, res) => res.sendFile(__dirname + "/html/index.html"));
 
-io.on('connection', function(socket){
-   console.log('a user connected');
+io.on("connection", function(socket) {
+  console.log("a user connected");
 });
+
+// Makes the db connection to firebase
+firebase.initFirebase();
 
 http.listen(port, () => console.log(`Example app listening on port ${port}!`));
