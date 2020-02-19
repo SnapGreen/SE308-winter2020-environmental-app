a1a7892ba9752b70278da9a614f7e7f93d134918
Kalen <ktwmgoo@gmail.com>
2020-02-19 15:12:56 -0800

    Created Firebase class and two query methods

--- Backend/firebase/index.js
+++ Backend/firebase/index.js
@@ -4,28 +4,56 @@ var admin = require("firebase-admin");
 // ** DO NOT SHARE **
 var serviceAccount = require("./se-environmental-app-firebase-adminsdk-fx69d-601b8c5684");
 
-function initFirebase() {
-  // Makes the connection to Firebase
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://se-environmental-app.firebaseio.com"
-  });
-
-  // Connects to the cloud firestore
-  let db = admin.firestore();
-
-  // Queries and returns all the users in the db
-  db.collection("users")
-    .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.id, "=>", doc.data());
-      });
-    })
-    .catch(err => {
-      console.log("Error getting documents", err);
+class Firebase {
+  constructor() {
+    // Makes the connection to Firebase
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://se-environmental-app.firebaseio.com"
     });
+
+    // Connects to the cloud firestore
+    this.db = admin.firestore();
+  }
+
+  getUser(username) {
+    username = username.toLowerCase();
+
+    // Queries for the specified user
+    return this.db
+      .collection("users")
+      .where("username", "==", username)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return null;
+        }
+        // console.log(snapshot.docs[0].data());
+        // if username matches, returns the document for that user
+        return snapshot.docs[0].data();
+      })
+      .catch(err => {
+        console.log("Error getting documents", err);
+      });
+    // return user;
+  }
+
+  getAllUsers() {
+    // Queries and returns all the users in the db
+    this.db
+      .collection("users")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log(doc.id, "=>", doc.data());
+        });
+      })
+      .catch(err => {
+        console.log("Error getting documents", err);
+      });
+  }
 }
 
 // Exports firebase functions to be used in a different file
-exports.initFirebase = initFirebase;
+exports.Firebase = Firebase;
