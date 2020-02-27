02bde8838186b3710731e42441e6648a71978eee
Kalen <ktwmgoo@gmail.com>
2020-02-26 18:00:21 -0800

    Create-user functionality

--- Backend/server/app/index.js
+++ Backend/server/app/index.js
@@ -63,7 +63,7 @@ app.get("/", function(req, res) {
 });
 
 // Used to create a new user
-app.post("/user", async function(req, res) {
+app.post("/users", async function(req, res) {
   if (!req.body) {
     res.status(401).json({
       message: "No req.body present"
@@ -91,37 +91,35 @@ app.post("/login", async function(req, res) {
       message: "No req.body present"
     });
     console.log("No req.body present");
-  }
-  else if (req.body.name && req.body.password) {
-    try{
-       // checks the database and then determines if the passwords matchs
-       let user = await FIREBASE.getUser(req.body.name);
+  } else if (req.body.name && req.body.password) {
+    try {
+      // checks the database and then determines if the passwords matchs
+      let user = await FIREBASE.getUser(req.body.name);
 
-       if (!user) {
-         res.status(401).json({
-           message: "username not found"
-         });
-       }
+      if (!user) {
+        res.status(401).json({
+          message: "username not found"
+        });
+      }
 
-       if (user.password === req.body.password) {
-         //   // var payload = {
-         //   //   id: user.id
-         //   // };
-         //   // var token = jwt.sign(payload, jwtOptions.secretOrKey);
-         res.json({
-           message: "ok"
-           // token: tokens
-         });
-         console.log("ok");
-       } else {
-         res.status(401).json({
-           message: "passwords do not match"
-         });
-         console.log("passwords do not match");
-       }
-    }
-    catch(err){
-       console.log("username not found");
+      if (user.password === req.body.password) {
+        //   // var payload = {
+        //   //   id: user.id
+        //   // };
+        //   // var token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({
+          message: "ok"
+          // token: tokens
+        });
+        console.log("ok");
+      } else {
+        res.status(401).json({
+          message: "passwords do not match"
+        });
+        console.log("passwords do not match");
+      }
+    } catch (err) {
+      console.log("username not found");
     }
   }
 });
