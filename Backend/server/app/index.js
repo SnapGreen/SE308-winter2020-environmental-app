bbf4db7bfa007f238fca1eed363d436ecc3551e4
jptwedt <jptwedt@calpoly.edu>
2020-03-05 22:44:48 -0800

    added rawdata folders to server added server scripts to convert csv to json server barcode scan api tested and added

--- Backend/server/app/index.js
+++ Backend/server/app/index.js
@@ -92,29 +92,70 @@ app.post("/login", async function(req, res) {
   } else if (req.body.name && req.body.password) {
     try {
       // checks the database and then determines if the passwords matchs
+      console.log("user login attempt");
       let user = await FIREBASE.getUser(req.body.name);
 
       if (!user) {
         res.status(401).json({
           message: "username not found"
         });
+        console.log("username not found");
       }
 
       if (user.password === req.body.password) {
         res.json({
           message: "ok"
         });
+        console.log("ok");
       } else {
         res.status(401).json({
           message: "passwords do not match"
         });
+        console.log("passwords do not match");
       }
     } catch (err) {
       res.status(401).json({
         message: "Login Error"
       });
+      console.log("Login Error");
     }
   }
+});
+
+app.get("/barcode", async function(req, res) {
+  console.log("barcode scan request received");
+  if (!req.body) {
+    res.status(401).json({
+      description: "No req.body present"
+    });
+    console.log("No req.body present");
+  }
+  else if(req.body.barcode == "123456789012"){
+    res.status(302).json({
+       description: "barcode found"
+    });
+    console.log("barcode request %s found", req.body.barcode);
+  }
+  else{
+    res.status(404).json({
+       description: "barcode not found"
+    });
+    console.log("barcode scan request %s not found", req.body.barcode);
+  }
+
+   /* Firebase part to be added later
+  let user = await FIREBASE.getUser(req.body.username);
+  if (user) {
+    res.json({
+      message: "User already exists. Please try a different username."
+    });
+  } else {
+    let newId = await FIREBASE.createUser(req.body);
+    res.json({
+      message: `New user added. Id is ${newId}`
+    });
+  }
+  */
 });
 
 /* These are for later, when we integrate real-time game comms
