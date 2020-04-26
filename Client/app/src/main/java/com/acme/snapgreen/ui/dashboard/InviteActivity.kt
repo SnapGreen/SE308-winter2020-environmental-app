85f01145c4d127cf38213b959c3a02853842c266
Destin Estrela <destin.estrela@gmail.com>
2020-04-26 18:18:44 -0400

    Added template for scrolling friends list view

--- Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/InviteActivity.kt
+++ Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/InviteActivity.kt
@@ -1,15 +1,36 @@
 package com.acme.snapgreen.ui.dashboard
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.acme.snapgreen.R
 
 class InviteActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(applicationContext, "Activity launched!", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_invite)
+        val myDataset = arrayOf("Yeehaw", "Yippe!", "Yeehaw", "Yippe!", "Yeehaw", "Yippe!", "Yeehaw", "Yippe!", "Yeehaw", "Yippe!")
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.friends_recycle_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
 }
+
---
+++ Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/MyAdapter.kt
@@ -0,0 +1,46 @@
+package com.acme.snapgreen.ui.dashboard
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.acme.snapgreen.R
+
+
+class MyAdapter(private val myDataset: Array<String>) :
+
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(view: View, val textView: TextView) : RecyclerView.ViewHolder(view)
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.user_display, parent, false)
+
+        val textView = view.findViewById<TextView>(R.id.user_display_name)
+        //val textView = view.findViewById<TextView>(R.id.user_display_name)
+        // set the view's size, margins, paddings and layout parameters
+
+        return MyViewHolder(view, textView)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = myDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
