c893d5a5fb1d105c1752d4bd5249ca9454c59a9b
Destin Estrela <destin.estrela@gmail.com>
2020-05-01 21:42:43 -0400

    (UI ONLY) added friends list view and ability to add friends

--- Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/FriendAdapter.kt
+++ Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/FriendAdapter.kt
@@ -1,5 +1,6 @@
 package com.acme.snapgreen.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,39 +9,45 @@ import androidx.recyclerview.widget.RecyclerView
 import com.acme.snapgreen.R
 
 
-class MyAdapter(private val myDataset: Array<String>) :
+class FriendAdapter(private val friendsList: List<String>) :
 
-    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    RecyclerView.Adapter<FriendAdapter.FriendEntryHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(view: View, val textView: TextView) : RecyclerView.ViewHolder(view)
+    class FriendEntryHolder(view: View, val name: TextView, val score: TextView) :
+        RecyclerView.ViewHolder(view)
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): MyViewHolder {
+    ): FriendEntryHolder {
         // create a new view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.user_display, parent, false)
 
-        val textView = view.findViewById<TextView>(R.id.user_display_name)
+        val nameTextView = view.findViewById<TextView>(R.id.user_display_name)
+        val scoreTextView = view.findViewById<TextView>(R.id.user_score)
         //val textView = view.findViewById<TextView>(R.id.user_display_name)
         // set the view's size, margins, paddings and layout parameters
 
-        return MyViewHolder(view, textView)
+        return FriendEntryHolder(view, nameTextView, scoreTextView)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: FriendEntryHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = myDataset[position]
+        holder.name.text = friendsList[position]
+
+        //TODO: save and update score properly
+        holder.score.text = "Score: " + (90..700).shuffled().first().toString()
     }
 
     // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = myDataset.size
+    override fun getItemCount() = friendsList.size
 }
