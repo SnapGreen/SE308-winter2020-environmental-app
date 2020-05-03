d0504d8ab20040b87870797a886713be82723de6
Zachary Reyes <zreyes@Zachs-MacBook-Pro.local>
2020-05-03 14:54:05 -0700

    Add profile pic from gallery or camera

--- Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/ProfileActivity.kt
+++ Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/ProfileActivity.kt
@@ -1,17 +1,24 @@
 package com.acme.snapgreen.ui.dashboard
 
+import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.acme.snapgreen.R
-import com.acme.snapgreen.ui.login.LoginActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.user_display.*
+
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -29,6 +36,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Toast.makeText(applicationContext, "Activity launched!", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_profile)
+
 
         mUserImage = findViewById(R.id.profilePicImageView)
         mFirstName = findViewById(R.id.profileFirstName)
@@ -73,8 +81,70 @@ class ProfileActivity : AppCompatActivity() {
             }
 
 //            updateUser(firstName, lastName, phoneNum, email, password)
+
         }
 
+        mUserImage.setOnClickListener {
+            selectImage(this)
+        }
+
+    }
+
+    private fun selectImage(context: Context) {
+        val options =
+            arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(context)
+        builder.setTitle("Choose your profile picture")
+        builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+            if (options[item] == "Take Photo") {
+                val takePicture =
+                    Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(takePicture, 0)
+            } else if (options[item] == "Choose from Gallery") {
+                val pickPhoto = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+                startActivityForResult(pickPhoto, 1)
+            } else if (options[item] == "Cancel") {
+                dialog.dismiss()
+            }
+        })
+        builder.show()
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_CANCELED) {
+            when (requestCode) {
+                0 -> if (resultCode == Activity.RESULT_OK && data != null) {
+                    val selectedImage = data.extras["data"] as Bitmap
+                    imageView.setImageBitmap(selectedImage)
+                }
+                1 -> if (resultCode == Activity.RESULT_OK && data != null) {
+                    val selectedImage: Uri? = data.data
+                    val filePathColumn =
+                        arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor: Cursor? = contentResolver.query(
+                            selectedImage,
+                            filePathColumn, null, null, null
+                        )
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath: String = cursor.getString(columnIndex)
+                            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+                            cursor.close()
+                        }
+                    }
+                }
+            }
+        }
     }
 
 //    private fun updateUser(
