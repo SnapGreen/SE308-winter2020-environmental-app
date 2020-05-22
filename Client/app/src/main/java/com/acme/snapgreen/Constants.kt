9de3ff292adb12e70214c1d1798bcf5e16ba7282
Destin Estrela <destin.estrela@gmail.com>
2020-05-22 14:11:31 -0400

    Readded server ip to constant (was local by accident)

--- Client/app/src/main/java/com/acme/snapgreen/Constants.kt
+++ Client/app/src/main/java/com/acme/snapgreen/Constants.kt
@@ -3,8 +3,6 @@ package com.acme.snapgreen
 class Constants {
 
     companion object {
-        //const val SERVER_URL = "http://34.209.125.52:8080"
-        const val SERVER_URL = "http://192.168.0.6:8080"
-
+        const val SERVER_URL = "http://34.209.125.52:8080"
     }
 }
