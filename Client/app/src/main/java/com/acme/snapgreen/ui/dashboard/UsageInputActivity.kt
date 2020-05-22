6ff75bedb358c2d884b698d27f075644428a9087
Destin Estrela <destin.estrela@gmail.com>
2020-05-22 14:06:03 -0400

    Removed 'Activity Launched' toasts

--- Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/UsageInputActivity.kt
+++ Client/app/src/main/java/com/acme/snapgreen/ui/dashboard/UsageInputActivity.kt
@@ -43,7 +43,6 @@ class UsageInputActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(applicationContext, "Activity launched!", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_usage_input)
 
         val currentDateField = findViewById<TextView>(R.id.currentDate)
