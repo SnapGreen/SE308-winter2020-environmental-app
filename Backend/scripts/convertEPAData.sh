f5d6b1880c1137e5140670d5d84b18c23b8c3660
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-05-23 17:40:02 -0700

    diversified removal pattern files

--- Backend/scripts/convertEPAData.sh
+++ Backend/scripts/convertEPAData.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 SETTINGS="/home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/settings.txt"
 DATADIR=$(grep -oP '(?<=^DATADIR:).*' $SETTINGS)
-NEWDATADIR=$1
-EPADATASOURCE=$2
+NEWDATADIR="$1"
+EPADATASOURCE="$2"
 NEWDATAPATH="${NEWDATADIR}${EPADATASOURCE}"
-TEMPDIR=$(grep -oP '(?<=^TEMPDIR:).*' $SETTINGS)
+TMPDIR=$(grep -oP '(?<=^TMPDIR:).*' $SETTINGS)
+TMPFILE_END=$(grep -oP '(?<=^TMPFILE_END:).*' $SETTINGS)
+SAFER_PREFIX=$(echo $EPADATASOURCE | grep -oP ".*(?=.xls)")
+SAFERCSV_TMP="${TMPDIR}${SAFER_PREFIX}_csv${TMPFILE_END}"
 USAGE="\t\tUsage: ./convertEPAData.sh [OPTION] (use option -h for help)\n"
 HELP="${USAGE}\t\t**If no OPTION supplied, debug mode on (temp files remain)\n"
 HELP="${HELP}\t\t\t-b: bypass debug mode\n"
@@ -22,31 +25,53 @@ function checkSettings(){
    printf "\tNEWDATADIR: %s\n" "$NEWDATADIR"
    printf "\tEPADATASOURCE: %s\n" "$EPADATASOURCE"
    printf "\tNEWDATAPATH: %s\n" "$NEWDATAPATH"
+   printf "\tTMPDIR: %s\n" "$TMPDIR"
+   printf "\tTMPFILE_END: %s\n" "$TMPFILE_END"
+   printf "\tSAFER_PREFIX: %s\n" "$SAFER_PREFIX"
+   printf "\tSAFERCSV_TMP: %s\n" "$SAFERCSV_TMP"
    printf "\tUSAGE:\n"
    printf "$USAGE"
    printf "\tHELP:\n"
    printf "$HELP"
 }
 
-if [[ -n $1 ]] ; then
-   if [ "$1" == "-h" ] ; then
-      printf "$HELP"
-      fin=true
-   elif [ "$1" == "-f" ] || [ "$1" == "-b" ] ; then
-      debug=false
-      silent=true
-   elif [ "$1" == "-s" ] ; then
-      checkSettings
-      fin=true
-   else
-      printf "$USAGE"
-      fin=true
+function prepData(){
+   echo "prepping data"
+   dos2unix $1
+}
+
+function convertToCsv(){
+   #https://www.unix.com/shell-programming-and-scripting/156328-how-convert-xls-file-csv.html
+   echo "converting to csv"
+   xls2csv "$1" > "$2"
+}
+
+if [[ $# -gt 2 ]] ; then
+   if [ -n $3 ] ; then
+      if [ "$3" == "-b" ] ; then
+         debug=false
+      elif [ "$3" == "-h" ] ; then
+         printf "$HELP"
+         fin=true
+      elif [ "$3" == "-s" ] ; then
+         checkSettings
+         fin=true
+      else
+         printf "$USAGE"
+         fin=true
+      fi
    fi
+elif [[ $# -lt 2 ]] ; then
+   printf "$USAGE"
+   fin=true
 fi
 
 if [ "$fin" == "false" ] ; then
-   if [ "$silent" == "false" ] ; then
+   if [[ "$debug" == "true" ]] ; then
       checkSettings
    fi
+   
+   prepData "$NEWDATAPATH"
+   convertToCsv "$NEWDATAPATH" "$SAFERCSV_TMP"
 
 fi
