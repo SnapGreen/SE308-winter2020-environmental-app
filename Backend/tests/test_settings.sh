64c0fabecd84156d1c1425f43ac5699904aac8f0
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-06-01 16:58:21 -0700

    reworked test_settings a bit

--- Backend/tests/test_settings.sh
+++ Backend/tests/test_settings.sh
@@ -1,66 +1,6 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-   SETTINGS="/home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/settings.txt"
-   TESTDIR=$(grep -oP '(?<=^TESTDIR:).*' $SETTINGS)
-   TESTDIR="${TESTDIR}settings/"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/convertToJson.sh -s > "${TESTDIR}convertToJson_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/getFDAUpdate.sh -s > "${TESTDIR}getFDAUpdate_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/downloadData.sh fakefile fakeurl -s > "${TESTDIR}downloadData_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/distributeFiles.sh fakefile.zip -s > "${TESTDIR}distributeFiles_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/uploadToDB.sh -s > "${TESTDIR}uploadToDB_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/populateDB.sh -s > "${TESTDIR}populateDB_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/getEPAUpdate.sh -s > "${TESTDIR}getEPAUpdate_settings.out"
-   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/convertEPAData.sh fakedir/ fakefile.xls -s > "${TESTDIR}convertEPAData_settings.out"
-
-   diff "${TESTDIR}convertToJson_settings.out" "${TESTDIR}convertToJson_settings.txt" 
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 1
-   fi
-
-   diff "${TESTDIR}getFDAUpdate_settings.out" "${TESTDIR}getFDAUpdate_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 2
-   fi
-
-   diff "${TESTDIR}downloadData_settings.out" "${TESTDIR}downloadData_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 3
-   fi
-
-   diff "${TESTDIR}distributeFiles_settings.out" "${TESTDIR}distributeFiles_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 4
-   fi
-
-   diff "${TESTDIR}uploadToDB_settings.out" "${TESTDIR}uploadToDB_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 5
-   fi
-
-   diff "${TESTDIR}populateDB_settings.out" "${TESTDIR}populateDB_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 6
-   fi
-
-   diff "${TESTDIR}getEPAUpdate_settings.out" "${TESTDIR}getEPAUpdate_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 7
-   fi
-
-   diff "${TESTDIR}convertEPAData_settings.out" "${TESTDIR}convertEPAData_settings.txt"
-   res=$?
-   if [[ $res -ne 0 ]]; then
-      exit 8
-   fi
-elif [[ "$1" == "-t" ]]; then
+if [[ "$1" == "-t" ]]; then
    SETTINGS_NPM="./Backend/scripts/settings_npm.txt"
    TESTDIR=$(grep -oP '(?<=^TESTDIR:).*' $SETTINGS_NPM)
    TESTDIR="./Backend/tests/settings/"
@@ -121,6 +61,64 @@ elif [[ "$1" == "-t" ]]; then
       exit 8
    fi
 else
-   echo "Usage: ./test_settings.sh [-t]"
+   SETTINGS="/home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/settings.txt"
+   TESTDIR=$(grep -oP '(?<=^TESTDIR:).*' $SETTINGS)
+   TESTDIR="${TESTDIR}settings/"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/convertToJson.sh -s > "${TESTDIR}convertToJson_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/getFDAUpdate.sh -s > "${TESTDIR}getFDAUpdate_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/downloadData.sh fakefile fakeurl -s > "${TESTDIR}downloadData_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/distributeFiles.sh fakefile.zip -s > "${TESTDIR}distributeFiles_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/uploadToDB.sh -s > "${TESTDIR}uploadToDB_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/populateDB.sh -s > "${TESTDIR}populateDB_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/getEPAUpdate.sh -s > "${TESTDIR}getEPAUpdate_settings.out"
+   /home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scripts/convertEPAData.sh fakedir/ fakefile.xls -s > "${TESTDIR}convertEPAData_settings.out"
+
+   diff "${TESTDIR}convertToJson_settings.out" "${TESTDIR}convertToJson_settings.txt" 
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 1
+   fi
+
+   diff "${TESTDIR}getFDAUpdate_settings.out" "${TESTDIR}getFDAUpdate_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 2
+   fi
+
+   diff "${TESTDIR}downloadData_settings.out" "${TESTDIR}downloadData_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 3
+   fi
+
+   diff "${TESTDIR}distributeFiles_settings.out" "${TESTDIR}distributeFiles_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 4
+   fi
+
+   diff "${TESTDIR}uploadToDB_settings.out" "${TESTDIR}uploadToDB_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 5
+   fi
+
+   diff "${TESTDIR}populateDB_settings.out" "${TESTDIR}populateDB_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 6
+   fi
+
+   diff "${TESTDIR}getEPAUpdate_settings.out" "${TESTDIR}getEPAUpdate_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 7
+   fi
+
+   diff "${TESTDIR}convertEPAData_settings.out" "${TESTDIR}convertEPAData_settings.txt"
+   res=$?
+   if [[ $res -ne 0 ]]; then
+      exit 8
+   fi
 fi
 
