f9dd36cd4c9ec9f2f491f6d72634d696975526ef
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-06-01 14:10:44 -0700

    travis settings updated

--- Backend/tests/test_settings.sh
+++ Backend/tests/test_settings.sh
@@ -3,37 +3,69 @@ SETTINGS="/home/jtwedt/projSE308/SE308-winter2020-environmental-app/Backend/scri
 TESTDIR=$(grep -oP '(?<=^TESTDIR:).*' $SETTINGS)
 TESTDIR="${TESTDIR}settings/"
 
-./../scripts/convertToJson.sh -s > "${TESTDIR}convertToJson_settings.out"
-./../scripts/getFDAUpdate.sh -s > "${TESTDIR}getFDAUpdate_settings.out"
-./../scripts/downloadData.sh fakefile fakeurl -s > "${TESTDIR}downloadData_settings.out"
-./../scripts/distributeFiles.sh fakefile.zip -s > "${TESTDIR}distributeFiles_settings.out"
-./../scripts/uploadToDB.sh -s > "${TESTDIR}uploadToDB_settings.out"
-./../scripts/populateDB.sh -s > "${TESTDIR}populateDB_settings.out"
-./../scripts/getEPAUpdate.sh -s > "${TESTDIR}getEPAUpdate_settings.out"
-./../scripts/convertEPAData.sh fakedir/ fakefile -s > "${TESTDIR}convertEPAData_settings.out"
+./Backend/scripts/convertToJson.sh -s > "${TESTDIR}convertToJson_settings.out"
+./Backend/scripts/getFDAUpdate.sh -s > "${TESTDIR}getFDAUpdate_settings.out"
+./Backend/scripts/downloadData.sh fakefile fakeurl -s > "${TESTDIR}downloadData_settings.out"
+./Backend/scripts/distributeFiles.sh fakefile.zip -s > "${TESTDIR}distributeFiles_settings.out"
+./Backend/scripts/uploadToDB.sh -s > "${TESTDIR}uploadToDB_settings.out"
+./Backend/scripts/populateDB.sh -s > "${TESTDIR}populateDB_settings.out"
+./Backend/scripts/getEPAUpdate.sh -s > "${TESTDIR}getEPAUpdate_settings.out"
+./Backend/scripts/convertEPAData.sh fakedir/ fakefile -s > "${TESTDIR}convertEPAData_settings.out"
 
-echo "checking convertToJson.sh settings..."
+#echo "checking convertToJson.sh settings..."
 diff "${TESTDIR}convertToJson_settings.out" "${TESTDIR}convertToJson_settings.txt" 
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 1
+fi
 
-echo "checking getFDAUpdate.sh settings..."
+#echo "checking getFDAUpdate.sh settings..."
 diff "${TESTDIR}getFDAUpdate_settings.out" "${TESTDIR}getFDAUpdate_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 2
+fi
 
-echo "checking downloadData.sh settings..."
+#echo "checking downloadData.sh settings..."
 diff "${TESTDIR}downloadData_settings.out" "${TESTDIR}downloadData_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 3
+fi
 
-echo "checking distributeFiles.sh settings..."
+#echo "checking distributeFiles.sh settings..."
 diff "${TESTDIR}distributeFiles_settings.out" "${TESTDIR}distributeFiles_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 4
+fi
 
-echo "checking uploadToDB.sh settings..."
+#echo "checking uploadToDB.sh settings..."
 diff "${TESTDIR}uploadToDB_settings.out" "${TESTDIR}uploadToDB_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 5
+fi
 
-echo "checking populateDB.sh settings..."
+#echo "checking populateDB.sh settings..."
 diff "${TESTDIR}populateDB_settings.out" "${TESTDIR}populateDB_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 6
+fi
 
-echo "checking getEPAUpdate.sh settings..."
+#echo "checking getEPAUpdate.sh settings..."
 diff "${TESTDIR}getEPAUpdate_settings.out" "${TESTDIR}getEPAUpdate_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 7
+fi
 
-echo "checking convertEPAData.sh settings..."
+#echo "checking convertEPAData.sh settings..."
 diff "${TESTDIR}convertEPAData_settings.out" "${TESTDIR}convertEPAData_settings.txt"
+res=$?
+if [[ $res -ne 0 ]]; then
+   exit 8
+fi
 
-echo "If there is no output after each of the checks above, settings check passed"
+#echo "If there is no output after each of the checks above, settings check passed"
