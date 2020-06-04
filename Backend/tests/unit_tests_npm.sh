3eaec3e9f178b2bae8ee10413cfb90cb403ee2f0
jtwedt <27270276+jptwedt@users.noreply.github.com>
2020-06-04 01:21:43 -0700

    typo in basepath for unit_test_npm

--- Backend/tests/unit_tests_npm.sh
+++ Backend/tests/unit_tests_npm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>$1 && pwd)"
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 testEquality(){
    assertEquals 1 1
